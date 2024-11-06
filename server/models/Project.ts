b017d7290adb2726df623cf1ec4ab69f57775fc5
harcop <harcopdrew@gmail.com>
2024-11-06 05:13:05 +0000

    add field virtual

--- server/models/Project.ts
+++ server/models/Project.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const projectSchema = new mongoose.Schema({
-	author: {
+	userId: {
 		type: String,
 		required: true,
 	},
@@ -25,19 +25,48 @@ const projectSchema = new mongoose.Schema({
 		type: String,
 		required: false,
 	},
-	link: {
+	site: {
 		type: String,
 		required: false,
 	},
 	likes: {
 		type: [String],
-		required: false,
+		default: [],
 	},
 	forks: {
 		type: [String],
-		required: false,
+		default: [],
 	},
+}, { timestamps: true });
+
+// Virtual for author (to be populated separately)
+projectSchema.virtual('author', {
+	ref: 'User',
+	localField: 'userId',
+	foreignField: '_id',
+	justOne: true,
 });
+
+// Virtual for totalLikes
+projectSchema.virtual('totalLikes').get(function () {
+	return this.likes.length;
+});
+
+// Virtual for totalForks
+projectSchema.virtual('totalForks').get(function () {
+	return this.forks.length;
+});
+
+// Virtual for totalComments
+projectSchema.virtual('totalComments', {
+	ref: 'Comment',
+	localField: '_id',
+	foreignField: 'projectId',
+	count: true,
+});
+
+projectSchema.set('toJSON', { virtuals: true });
+projectSchema.set('toObject', { virtuals: true });
 
 const projectModel = mongoose.model('Project', projectSchema);
 
